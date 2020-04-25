8742e8d760cf9f116fcc4d32dde4941af39c449f
Eduard <eduard.kamburjan@gmx.de>
2020-04-25 12:24:32 +0200

    Started on translation

--- src/main/kotlin/org/abs_models/chisel/main/Main.kt
+++ src/main/kotlin/org/abs_models/chisel/main/Main.kt
@@ -2,10 +2,7 @@
 
 package org.abs_models.chisel.main
 
-import abs.frontend.ast.ASTNode
-import abs.frontend.ast.ClassDecl
-import abs.frontend.ast.DataConstructorExp
-import abs.frontend.ast.Exp
+import abs.frontend.ast.*
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
@@ -72,14 +69,14 @@ class Main : CliktCommand() {
         verbosity = Verbosity.values()[verbose]
         outPath = "$out"
 
-        output("Crowbar  : loading files....")
+        output("Chisel  : loading files....")
         val input = filePath.map{ File(it.toString()) }
         if(input.any { !it.exists() }) {
             System.err.println("file not found: $filePath")
             exitProcess(-1)
         }
 
-        output("Crowbar  : loading ABS model....")
+        output("Chisel  : loading ABS model....")
         val model = try {
             abs.frontend.parser.Main().parse(input.map { it.toString() }.toTypedArray())
         } catch (e : Exception) {
@@ -101,6 +98,13 @@ class Main : CliktCommand() {
                             val pre = extractSpec(cDecl, "Requires")
                             val inv =  extractSpec(cDecl.physical, "ObjInv")
                             println("Chisel  : Extracted precondition $pre and invariant $inv for ${tt.path}")
+                            val ph = cDecl.physical
+                            //phyiscal block
+                            for(mDecl in cDecl.methods){
+                                println("Chisel  : Found method ${mDecl.methodSig.name}")
+                                println("Chisel  :        with initial ${translateGuard(extractInitial(mDecl))}")
+                                println("Chisel  :        with implementation ${extractImpl(mDecl)}")
+                            }
                         }
                         else throw Exception("non-physical classes not supported yet")
                     }
@@ -131,5 +135,79 @@ fun<T : ASTNode<out ASTNode<*>>?> extractSpec(decl : ASTNode<T>, expectedSpec : 
     return ret
 }
 
+fun extractInitial(mImpl : MethodImpl) : Guard?{
+    val lead = mImpl.block.getStmt(0)
+    return if(lead is AwaitStmt){
+        lead.guard
+    }else null
+}
 
+
+fun extractImpl(mImpl : MethodImpl) : String{
+    val init = if(extractInitial(mImpl) == null) 0 else 1
+    val sofar = emptyList<String>().toMutableList()
+    for(i in init .. mImpl.block.numStmt){
+        sofar += translateStmt(mImpl.block.getStmt(i))
+    }
+    return sofar.joinToString(";")
+}
+
+fun translateStmt(stmt: Stmt?) : String{
+    if(stmt == null) return "skip"
+    when(stmt){
+        is AssignStmt -> {
+            when(stmt.getChild(2)){
+                is PureExp ->
+                    return stmt.`var`.toString() + " := "+ translateExpr(stmt.getChild(2) as PureExp)
+                else -> {throw Exception("Translation not supported yet : ${stmt.getChild(2)}")}
+            }
+        }
+        is IfStmt -> {
+            return "if(${translateExpr(stmt.condition)}) ${translateStmt(stmt.then)} else  ${translateStmt(stmt.`else`)} "
+        }
+        is ExpressionStmt -> {
+            return "skip"
+        }
+        is Block -> {
+            return stmt.stmts.joinToString(";") { translateStmt(it) }
+        }
+        else -> {throw Exception("Translation not supported yet: $stmt")}
+    }
+}
+
+fun translateExpr(exp: Exp) : String{
+    when(exp) {
+        is DivMultExp -> return "(${translateExpr(exp.left)}/${translateExpr(exp.right)})"
+        is MultMultExp -> return "(${translateExpr(exp.left)}/${translateExpr(exp.right)})"
+        is ModMultExp -> return "(${translateExpr(exp.left)}/${translateExpr(exp.right)})"
+        is SubAddExp -> return "(${translateExpr(exp.left)}-${translateExpr(exp.right)})"
+        is AddAddExp -> return "(${translateExpr(exp.left)}+${translateExpr(exp.right)})"
+        is LTEQExp -> return "(${translateExpr(exp.left)}<=${translateExpr(exp.right)})"
+        is LTExp -> return "(${translateExpr(exp.left)}<${translateExpr(exp.right)})"
+        is GTExp -> return "(${translateExpr(exp.left)}>${translateExpr(exp.right)})"
+        is GTEQExp -> return "(${translateExpr(exp.left)}>=${translateExpr(exp.right)})"
+        is EqExp -> return "(${translateExpr(exp.left)}==${translateExpr(exp.right)})"
+        is NotEqExp -> return "(${translateExpr(exp.left)}!=${translateExpr(exp.right)})"
+        is OrBoolExp -> return "(${translateExpr(exp.left)}||${translateExpr(exp.right)})"
+        is AndBoolExp -> return "(${translateExpr(exp.left)}&&${translateExpr(exp.right)})"
+        is IntLiteral -> return "(${exp.content})"
+        is FieldUse -> return "(${exp})"
+        is VarUse -> return "(${exp})"
+        else -> {throw Exception("Translation not supported yet: $exp")}
+    }
+}
+
+
+fun translateGuard(exp: Guard?) : String{
+    if(exp == null) return "true"
+    return when(exp){
+        is DifferentialGuard -> translateGuard(exp.condition)
+        is ExpGuard -> translateExpr(exp.pureExp)
+        is AndGuard -> "(${translateGuard(exp.left)}) && (${translateGuard(exp.right)})"
+        is OrGuard -> "(${translateGuard(exp.left)}) || (${translateGuard(exp.right)})"
+        is DurationGuard -> "t >= ${translateExpr(exp.min)}"
+        is ClaimGuard -> "true"
+        else -> {throw Exception("Translation not supported yet: $exp")}
+    }
+}
 fun main(args:Array<String>) = Main().main(args)
